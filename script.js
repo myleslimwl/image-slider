a611245fdc7a838d1e7965108cdcb12483b323ca
myleslim <mousee1030@gmail.com>
2025-02-27 10:45:12 +0800

    created funcitons to show the slides

--- script.js
+++ script.js
@@ -10,14 +10,26 @@ function initializeSlider(){
   slides[slideIndex].style.display = 'block';
 
   if (slides.length > 0){
+    slides[slideIndex].classList.add('displaySlide');
     intervalID = setInterval(nextSlide, 3000);
   }
 }
 
 function prevSlide(){
-
+  slideIndex--;
+  showSlide(slideIndex);
 }
 
 function nextSlide(){
+  slideIndex++;
+  showSlide(slideIndex);
+}
 
+function showSlide(index){
+
+  slides.forEach(slide => {
+    slide.classList.remove('displaySlide');
+  });
+
+  slides[index].classList.add('displaySlide');
 }
